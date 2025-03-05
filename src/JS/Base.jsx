1442f08bec3c137857c536502792d6c9a948b33b
Pablo Goulart <pablogoulart02@gmail.com>
2025-03-04 21:29:36 -0300

    javascript pronto

--- src/JS/Base.jsx
+++ src/JS/Base.jsx
@@ -6,36 +6,46 @@ let amount = document.querySelector(".amount");
 let total = document.querySelector(".total");
 let selectedTip = 0;
 
-function Sum()
-{
+function Sum() {
     let value = (Number(bill.value) * Number(selectedTip.replace('%', '')) / 100) + Number(bill.value);
     return (Number(value) / numberPeople.value).toFixed(2);
 }
 
-function tipAmount()
-{
+function tipAmount() {
     return Math.floor((Number(bill.value) * Number(selectedTip.replace('%', '')) / 100) / numberPeople.value * 100) / 100;
 }
 
 btn.forEach(button => {
     button.addEventListener("click", (event) => {
-        if (numberPeople.value > 0)
-        {
-            selectedTip = event.target.textContent;
+        if (numberPeople.value > 0) {
+            selectedTip = event.target.textContent;  
+            custom.value = '';  
             total.textContent = Sum();
             amount.textContent = tipAmount();
-        }
-        else
-        {
+            document.querySelector(".error").style.display = 'none';
+            numberPeople.style.borderColor = "";
+        } else {
             document.querySelector(".error").style.display = 'inline';
             numberPeople.style.borderColor = '#B48372';
         }
     });
 });
 
-document.addEventListener("click", () => {
-    if (bill.value > 0 && numberPeople.value > 0)
-    {
-        total.textContent = Sum(bill, custom.value, numberPeople);
+custom.addEventListener("input", () => {
+    if (custom.value !== "") { 
+        selectedTip = custom.value; 
+        total.textContent = Sum(); 
+        amount.textContent = tipAmount();
     }
-})
+});
+
+document.querySelector(".reset").addEventListener("click", () => {
+    bill.value = "";
+    numberPeople.value = "";
+    selectedTip = 0;  
+    custom.value = "";  
+    amount.textContent = "$0.00";
+    total.textContent = "$0.00";
+    document.querySelector(".error").style.display = 'none';
+    numberPeople.style.borderColor = "";
+});
